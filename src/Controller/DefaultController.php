2cef3ff46a069de787fa97dd68b37e7e7dff9390
Sara Nowak <nowak.es10@gmail.com>
2020-11-23 22:33:41 +0100

    routing and connection to the database

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,12 +1,51 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController{
+class DefaultController extends AbstractController{
+    /**
+     * @Route("/", name="article_list")
+     */
     public function index(){
-        return new Response('<html><body>Hello</body></html>');
 
-}
+        $articles = ['Article 1', 'Article 2'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)-> findAll();
+        return $this -> render('articles/index.html.twig', array ('articles' => $articles));
+
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+
+    public function show ($id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this -> render('articles/show.html.twig', array ('article'=>$article));
+    }
+
+
+    ///**
+     //* @Route("/article/save")
+    // */
+    /*public function save(){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Article Two');
+        $article->setBody('This is the body for article two');
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response('Saves an article with the id of'.$article->getId());
+
+    }*/
+
 }
 
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,1 +1,66 @@
 <?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBody($body): void
+    {
+        $this->body = $body;
+    }
+
+
+}
